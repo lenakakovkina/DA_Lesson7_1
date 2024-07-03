0016b1e6986d5bb1c63bddc5610cf66a9c8f35d8
Samsung <elenakakovkina89@gmail.com>
2024-07-03 21:56:51 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿int minValue = 1;
+int maxValue = 10;
+int value = GetNumberFromUser(minValue, maxValue);
+Console.WriteLine($"User input the next value:{value}");
+
+
+
+int GetNumberFromUser(int min, int max)
+{
+    int UserValue;
+    Console.WriteLine($"Input value from {min} to {max}");
+    UserValue = int.Parse(Console.ReadLine());
+
+    if (!(UserValue >= min && UserValue <= max))
+    {
+        do
+        {
+            Console.WriteLine("Error. Try one more time.");
+            Console.WriteLine($"Input value from {min} to {max}");
+            UserValue = int.Parse(Console.ReadLine());
+        }
+        while (!(UserValue >= min && UserValue <= max));
+        return UserValue;
+
+    }
+    else
+    {
+        return UserValue;
+    }
+
+}
+
